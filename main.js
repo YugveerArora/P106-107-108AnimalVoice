9f484e8ff5a38fdc13ad5c017241442232f5d98b
YugveerArora <78415770+YugveerArora@users.noreply.github.com>
2022-07-05 13:13:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// https://teachablemachine.withgoogle.com/models/pA87G7bf4/model.json
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true})
+    classifier=ml5.soundClassifier(" https://teachablemachine.withgoogle.com/models/pA87G7bf4/model.json",modelReady)
+}
+
+function modelReady(){
+    classifier.classify(gotResults)
+}
